cc308f464fee9d38c3337554f16ea46630238270
felixyh <felixyanghui@gmail.com>
2023-02-20 20:03:28 +0800

    update

--- CloudFormation/Scripts/ctf_0_basic_functions.py
+++ CloudFormation/Scripts/ctf_0_basic_functions.py
@@ -26,8 +26,63 @@ def get_stack_status(stack_name, region='us-east-2'):
     return False
 
 
-def update_stack(stack_name, region='us-east-2'):
-    pass
+def create_update_stack(stack_name, template_path, region='us-east-2', parameters=None):
+    client = boto3.client('cloudformation', region_name=region)
+    if get_stack_status(stack_name, region=region):
+        try:
+            response = client.update_stack(
+                StackName=stack_name,
+                Parameters=parameters if parameters else [],
+                Capabilities=[
+                    'CAPABILITY_IAM',
+                    'CAPABILITY_AUTO_EXPAND',
+                    'CAPABILITY_NAMED_IAM'
+                ],
+                TemplateBody=open(template_path, encoding='UTF-8').read()
+            )
+            try:
+                status_code = response.get('ResponseMetadata').get('HTTPStatusCode')
+                if status_code == 200:
+                    return "一切正常"
+                else:
+                    print("出现错误")
+                    return status_code
+            except Exception as e:
+                return f"出现错误: {str(e)}"
+        except botocore.exceptions.ClientError as e:
+            if 'No updates are to be performed' in str(e):
+                print('无需更新')
+            print(e)
+    else:
+        try:
+            response = client.create_stack(
+                StackName=stack_name,
+                Parameters=parameters if parameters else [],
+                Capabilities=[
+                    'CAPABILITY_IAM',
+                    'CAPABILITY_AUTO_EXPAND',
+                    'CAPABILITY_NAMED_IAM'
+                ],
+                TemplateBody=open(template_path, encoding='UTF-8').read(),
+                Tags=[
+                    {
+                        'Key': 'Name',
+                        'Value': stack_name
+                    },
+                ],
+            )
+            try:
+                status_code = response.get('ResponseMetadata').get('HTTPStatusCode')
+                if status_code == 200:
+                    return "一切正常"
+                else:
+                    print("出现错误")
+                    return status_code
+            except Exception as e:
+                return f"出现错误: {str(e)}"
+
+        except Exception as e:
+            print(f'出现错误:{str(e)}')
 
 
 
