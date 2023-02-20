57c63c85084479d67ba1a3ccd28e7c52c187bcee
felixyh <felixyanghui@gmail.com>
2023-02-20 18:39:25 +0800

    update

--- CloudFormation/Scripts/ctf_0_basic_functions.py
+++ CloudFormation/Scripts/ctf_0_basic_functions.py
@@ -15,4 +15,25 @@ def get_stack_status(stack_name, region='us-east-2'):
         elif s.get('StackName') == stack_name and s.get('StackStatus') == 'UPDATE_ROLLBACK_COMPLETE':
             return True
         elif s.get('StackName') == stack_name and s.get('StackStatus') == 'ROLLBACK_COMPLETE':
-            
+            delete_stack(stack_name, region=region)
+            print('等待五秒删除处于\'ROLLBACK_COMPLETE\'状态的Stack')
+            time.sleep(5)
+            return False
+        elif s.get('StackName') == stack_name and s.get('StackStatus') == 'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS':
+            return True
+        elif s.get('StackName') == stack_name and s.get('StackStatus') == 'DELETE_IN_PROGRESS':
+            return True
+    return False
+
+
+def update_stack(stack_name, region='us-east-2'):
+    pass
+
+
+
+def delete_stack(stack_name, region='us-east-2'):
+    client = boto3.client('cloudformation', region_name=region)
+    response = client.delete_stack(
+        StackName=stack_name,
+    )
+    return response
